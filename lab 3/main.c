d74d8a816f73f588d1ce74b5c776b7ed68d30116
Pavel-Kruglikov <79210788+Pavel-Kruglikov@users.noreply.github.com>
2021-05-31 19:12:11 +0300

    Update main.c

--- lab 3/main.c
+++ lab 3/main.c
@@ -1,57 +1,94 @@
-﻿// ConsoleApplication6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// ConsoleApplication8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <math.h>
 
-double fact(int n) {
-    if (n == 0)
-        return 0;
-    if (n == 1)
-        return 1;
-    return (double)(n * fact(n - 1));
-}
-
-double factorial(int n) {
-    int i;
-    long double fact = 1;
-    if (N < 0)
-    {
-        return 0;
-    }
-    if (N == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        for (i = 1; i < N; i++)
-        {
-            fact *= i;
-        }
-    }
-}
-
-int res(int x, double e) {
-    int n = 0;
-    double curr = 0;
-    do {
-        n++;
-        curr += pow(-1, n - 1) * (pow(x, 2 * n - 1) / fact(2 * n - 1));
-    } 	while (fabs(sin(x) - curr) >= e);
-    return n;
-}
-
-int main()
+int check(void)
 {
-    double epsilon, x;
-    printf("Enter x:");
-    scanf("%lf", &x);
-    printf("Enter epsilon:");
-    scanf("%lf", &epsilon);
-    printf("n=%d", res(x, epsilon));
-    return 0;
+	int sum, flag; char a;
+	flag = 0;
+	while (flag == 0) {
+		sum = 0; a = '0';
+		while ((a <= '9') && (a >= '0')) {
+			scanf_s("%c", &a);
+			if (a == '\n') {
+				break;
+			}
+			sum = sum * 10 + (a - '0');
+		}
+		if (a != '\n') {
+			printf("THE ENTERED VALUE IF INVALID, PLEASE TRY AGAIN\n");
+			while (a != '\n') {
+				scanf_s("%c", &a);
+			}
+		}
+		else {
+			flag = 1;
+			break;
+		}
+	}
+	return sum;
+}
+
+int main(void)
+{
+	int n, m, i, j, player, x, y; int** a;
+	puts("Enter N:");
+	n = check();
+	puts("Enter M:");
+	m = check();
+	a = (int**)malloc(n * sizeof(int*));
+	for (i = 0; i < n; i++)
+		a[i] = (int*)malloc(m * sizeof(int));
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+			a[i][j] = 1;
+	a[n - 1][0] = -1;
+	player = 1;
+	while (1)
+	{
+		printf("Player number %d chooses a piece.\n", player);
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++)
+				printf("%3d", a[i][j]);
+			printf("\n");
+		}
+		printf("Choose a piece. ");
+		printf("Enter the coordinate of the line: ");
+		x = check();
+		x--;
+		printf("Enter the coordinate of the column: ");
+		y = check();
+		y--;
+		if ((x >= n) || (y >= m) || (x < 0) || (y < 0)) {
+			printf("Getting out of bounds\n");
+		}
+		else {
+			if (a[x][y] == 0) {
+				printf("The piece's already gone.\n");
+			}
+			if (a[x][y] == 1) {
+				for (i = 0; i <= x; i++)
+					for (j = y; j < m; j++)
+						a[i][j] = 0;
+				if (player == 2) {
+					player = 1;
+				}
+				else {
+					player = 2;
+				}
+			}
+			if (a[x][y] == -1) {
+				break;
+			}
+		}
+	}
+	printf("Player %d lost \n", player);
+	free(a);
+	return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
